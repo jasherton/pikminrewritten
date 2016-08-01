6de9b559652370c4b674289a5c28517c4130c8df
Gavitro <gavitrothe2nd@gmail.com>
2016-07-31 23:18:19 -0700

    added the Master Onion

---
+++ entities/pik_masteronion/shared.lua
@@ -0,0 +1,9 @@
+ENT.Type 			= "anim";
+ENT.Base 			= "base_anim";
+ENT.PrintName		= "Onion (Master)";
+ENT.Author			= "jasherton";
+ENT.Purpose			= "eck";
+ENT.Category		= "Pikmin";
+
+ENT.Spawnable			= true;
+ENT.AdminSpawnable		= true;
